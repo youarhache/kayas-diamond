741c4bf3c0485dc826f3134330c5ed26c97f3c82
youarhache <youarhache@gmail.com>
2022-01-06 20:28:04 +0100

    printing diamond with D plus refactoring

--- diamond.py
+++ diamond.py
@@ -1,7 +1,13 @@
-def print_diamond(letter):
-    if letter == "A":
+import string
+
+
+def print_diamond(input_letter):
+    if input_letter == "A":
         return "A"
-    elif letter == "B":
-        return " A \nB B\n A "
-    else:
-        return "  A  \n B B \nC   C\n B B \n  A  "
+    output = ["A"]
+    for i, letter in enumerate(string.ascii_uppercase[1:]):
+        output = [" " + line+ " " for line in output]
+        output.append(letter+ (2*i + 1)*" " + letter)
+        if letter == input_letter:
+            output.extend(output[-2::-1])
+            return "\n".join(output)
--- test_diamond.py
+++ test_diamond.py
@@ -1,13 +1,13 @@
 from diamond import print_diamond
 
 def test_print_diamond_when_A():
-    result = print_diamond(letter="A")
+    result = print_diamond(input_letter="A")
 
     assert result == "A"
 
 
 def test_print_diamond_when_B():
-    result = print_diamond(letter="B")
+    result = print_diamond(input_letter="B")
 
     assert result == " A \n" +\
                      "B B\n" +\
@@ -15,10 +15,22 @@ def test_print_diamond_when_B():
 
 
 def test_print_diamond_when_C():
-    result = print_diamond(letter="C")
+    result = print_diamond(input_letter="C")
 
     assert result == "  A  \n" +\
                      " B B \n" +\
                      "C   C\n" +\
                      " B B \n" +\
                      "  A  "
+
+
+def test_print_diamond_when_D():
+    result = print_diamond(input_letter="D")
+
+    assert result == "   A   \n" +\
+                     "  B B  \n" +\
+                     " C   C \n" +\
+                     "D     D\n" +\
+                     " C   C \n" +\
+                     "  B B  \n" +\
+                     "   A   "
