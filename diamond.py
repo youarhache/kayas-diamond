3f373806a951dffdead078b403a622f1f32473d3
youarhache <youarhache@gmail.com>
2022-01-06 20:06:20 +0100

    print diamiond with C

--- diamond.py
+++ diamond.py
@@ -1,4 +1,7 @@
 def print_diamond(letter):
     if letter == "A":
         return "A"
-    return " A \nB B\n A "
+    elif letter == "B":
+        return " A \nB B\n A "
+    else:
+        return "  A  \n B B \nC   C\n B B \n  A  "
--- test_diamond.py
+++ test_diamond.py
@@ -12,3 +12,13 @@ def test_print_diamond_when_B():
     assert result == " A \n" +\
                      "B B\n" +\
                      " A "
+
+
+def test_print_diamond_when_C():
+    result = print_diamond(letter="C")
+
+    assert result == "  A  \n" +\
+                     " B B \n" +\
+                     "C   C\n" +\
+                     " B B \n" +\
+                     "  A  "
